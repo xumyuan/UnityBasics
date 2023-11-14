ced3221b128ac6ee6e408e1d1805c6d165f7b176
xumy <947656035@qq.com>
2023-11-14 15:51:39 +0800

    3:Mathematical Surfaces

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -10,20 +10,41 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+
+    Transform[] points;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
     void Awake()
     {
         float step = 2f / resolution;
-        var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++)
-        {
-            Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;
 
-            point.localPosition = position;
+        points = new Transform[resolution * resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);
             point.localScale = scale;
             point.SetParent(transform, false);
+        }
+    }
+
+    void Update()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+        float time = Time.time;
+        float step = 2f / resolution;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            float v = (z + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
